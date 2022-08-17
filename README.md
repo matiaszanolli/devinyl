4135bb518248c317d9733e73004d977ef3bf5d5b
Matías Zanolli <gtd.inf@gmail.com>
2022-08-17 10:55:01 -0300

    General refactor and update.

--- README.md
+++ README.md
@@ -4,6 +4,66 @@ This project started as a proof of concept that old vynils could be recovered at
 
 It needs a reference audio. Preferably something that covers most of the audio spectrum without saturating will do fine, like most of 90's rock or pop records for example. But that's the thing. There's not an exact formula for every song, so feel free to experiment and check out the variation on the results!
 
-### Install - Requirements (Pip - Conda - Apt)
+### Requirements
 
-Really really soon!
+* Linux / Windows / macOS (not tested on this last one but should work)
+* SoX binaries (available as packages on Linux via Apt / dnf / pacman, must download from website in Windows / macOS)
+* ffmpeg binaries (same as above, though available in windows through chocolatey / Winget and macOS through brew)
+* (Windows only) check that SoX and ffmpeg are accessible through terminal, otherwise add their respective location to your user's PATH env variable.
+
+## Install - Requirements (Pip - Conda - Apt)
+
+### Conda
+
+Create a new environment for your devinyling to happen:
+
+```bash
+conda create --name devinyl python=3.7
+
+conda activate devinyl
+```
+
+### Pip
+
+Then follow the pip instructions ahead.
+
+install all necesary dependencies:
+
+```bash
+pip install -r requirements.txt
+```
+
+### Usage
+
+```
+devinyl.py [-h] [-b {16,24,32}] [--log LOG] [--no_limiter]
+           [--dont_normalize]
+           target_file reference_file
+
+positional arguments:
+    target                The track you want to start from
+    reference             Some reference track to enhance the base sound
+
+optional arguments:
+    -h, --help            show this help message and exit
+    -b {16,24,32}, --bit {16,24,32}
+                        The bit depth of your mastered result. 32 means 32-bit
+                        float
+    --no_limiter          Disables the limiter at the final stage of processing
+    --dont_normalize      Disables normalization, if --no_limiter is set.
+```
+## Extras
+
+### Thanks to these wonderful sites and repos!
+
+[Noise reduction gist]: https://github.com/dodiku/noise_reduction/blob/master/noise.py
+[Noise reduction topic]: http://dsp.stackexchange.com/search?q=noise+reduction/
+[matchering]: https://github.com/sergree/matchering
+[matchering-cli]: https://github.com/sergree/matchering-cli
+[anaconda]: https://www.anaconda.com/products/individual#Downloads
+[FFmpeg]: https://www.ffmpeg.org/download.html
+
+
+### If this helped you out, please buy me a coffee!
+
+https://www.buymeacoffee.com/matiaszanolli
--- devinyl.py
+++ devinyl.py
@@ -1,21 +1,20 @@
 import os
 import logging
-import math
 import random
 import sys
 from argparse import ArgumentParser
 from pathlib import Path
 
 import librosa
-from pysndfx import AudioEffectsChain
 import numpy as np
 import matchering as mg
 from matchering.core import Config
 from matchering.results import Result
-import python_speech_features
 import soundfile as sf
-import scipy as sp
 from tqdm import tqdm
+
+from .effects import (reduce_noise_centroid_mb, reduce_noise_centroid_s, reduce_noise_median, reduce_noise_mfcc_down, 
+                     reduce_noise_mfcc_up, reduce_noise_no_reduction, reduce_noise_power)
 
 # Thanks to all of these projects and pages for making this possible.
 
@@ -38,177 +37,6 @@ def read_file(file_name):
 
     return y, sr
 
-'''------------------------------------
-NOISE REDUCTION USING POWER:
-    receives an audio matrix,
-    returns the matrix after gain reduction on noise
-------------------------------------'''
-def reduce_noise_power(y, sr):
-
-    cent = librosa.feature.spectral_centroid(y=y, sr=sr)
-
-    threshold_h = round(np.median(cent))*1.4
-    threshold_l = round(np.median(cent))*0.2
-
-    less_noise = AudioEffectsChain().lowshelf(gain=-34.0, frequency=threshold_l, slope=0.7).highshelf(gain=-16.0, frequency=threshold_h, slope=0.6).limiter(gain=8.0)
-    y_clean = less_noise(y)
-
-    return y_clean
-
-
-'''------------------------------------
-NOISE REDUCTION USING CENTROID ANALYSIS:
-    receives an audio matrix,
-    returns the matrix after gain reduction on noise
-------------------------------------'''
-
-def reduce_noise_centroid_s(y, sr):
-    
-    s = np.abs(y)
-
-    cent = librosa.feature.spectral_centroid(y=s, sr=sr)
-
-    threshold_h = np.max(cent)
-    threshold_l = np.min(cent)
-
-    less_noise = AudioEffectsChain().lowshelf(gain=-12.0, frequency=threshold_l, slope=0.5).highshelf(gain=-12.0, frequency=threshold_h, slope=0.5).limiter(gain=6.0)
-
-    y_cleaned = less_noise(y)
-
-    return y_cleaned
-
-def reduce_noise_centroid_mb(y, sr):
-    y = np.abs(y)
-    
-    cent = librosa.feature.spectral_centroid(y=y, sr=sr)
-
-    threshold_h = np.max(cent)
-    threshold_l = np.min(cent)
-
-    less_noise = AudioEffectsChain().lowshelf(gain=-30.0, frequency=threshold_l, slope=0.5).highshelf(gain=-30.0, frequency=threshold_h, slope=0.5).limiter(gain=10.0)
-    less_noise = AudioEffectsChain().lowpass(frequency=threshold_h).highpass(frequency=threshold_l)
-    y_cleaned = less_noise(y)
-
-
-    cent_cleaned = librosa.feature.spectral_centroid(y=y_cleaned, sr=sr)
-    columns, channels, rows = cent_cleaned.shape
-    boost_h = math.floor(rows/3*2)
-    boost_l = math.floor(rows/6)
-    boost = math.floor(rows/3)
-
-    # boost_bass = AudioEffectsChain().lowshelf(gain=20.0, frequency=boost, slope=0.8)
-    boost_bass = AudioEffectsChain().lowshelf(gain=16.0, frequency=boost_h, slope=0.5).lowshelf(gain=-20.0, frequency=boost_l, slope=0.8)
-    y_clean_boosted = boost_bass(y_cleaned)
-
-    return y_clean_boosted
-
-
-'''------------------------------------
-NOISE REDUCTION USING MFCC:
-    receives an audio matrix,
-    returns the matrix after gain reduction on noise
-------------------------------------'''
-def reduce_noise_mfcc_down(y, sr):
-
-    hop_length = 512
-
-    ## librosa
-    # mfcc = librosa.feature.mfcc(y=y, sr=sr, reference, sr2, hop_length=hop_length, n_mfcc=13)
-    # librosa.mel_to_hz(mfcc)
-
-    ## mfcc
-    mfcc = python_speech_features.base.mfcc(y)
-    mfcc = python_speech_features.base.logfbank(y)
-    mfcc = python_speech_features.base.lifter(mfcc)
-
-    sum_of_squares = []
-    index = -1
-    for r in mfcc:
-        sum_of_squares.append(0)
-        index = index + 1
-        for n in r:
-            sum_of_squares[index] = sum_of_squares[index] + n**2
-
-    strongest_frame = sum_of_squares.index(max(sum_of_squares))
-    hz = python_speech_features.base.mel2hz(mfcc[strongest_frame])
-
-    max_hz = max(hz)
-    min_hz = min(hz)
-
-    speech_booster = AudioEffectsChain().highshelf(frequency=min_hz*(-1)*1.2, gain=-12.0, slope=0.6).limiter(gain=8.0)
-    y_speach_boosted = speech_booster(y)
-
-    return (y_speach_boosted)
-
-def reduce_noise_mfcc_up(y, sr):
-
-    hop_length = 512
-
-    ## librosa
-    # mfcc = librosa.feature.mfcc(y=y, sr=sr, reference, sr2, hop_length=hop_length, n_mfcc=13)
-    # librosa.mel_to_hz(mfcc)
-
-    ## mfcc
-    mfcc = python_speech_features.base.mfcc(y)
-    mfcc = python_speech_features.base.logfbank(y)
-    mfcc = python_speech_features.base.lifter(mfcc)
-
-    sum_of_squares = []
-    index = -1
-    for r in mfcc:
-        sum_of_squares.append(0)
-        index = index + 1
-        for n in r:
-            sum_of_squares[index] = sum_of_squares[index] + n**2
-
-    strongest_frame = sum_of_squares.index(max(sum_of_squares))
-    hz = python_speech_features.base.mel2hz(mfcc[strongest_frame])
-
-    max_hz = max(hz)
-    min_hz = min(hz)
-
-    speech_booster = AudioEffectsChain().lowshelf(frequency=min_hz*(-1), gain=12.0, slope=0.5).highshelf(frequency=min_hz*(-1)*1.2, gain=-12.0, slope=0.5)#.limiter(gain=8.0)
-    y_speach_boosted = speech_booster(y)
-
-    return (y_speach_boosted)
-
-'''------------------------------------
-NOISE REDUCTION USING MEDIAN:
-    receives an audio matrix,
-    returns the matrix after gain reduction on noise
-------------------------------------'''
-
-def reduce_noise_median(y, sr):
-    y = sp.signal.medfilt(y,3)
-    return (y)
-
-
-def reduce_noise_no_reduction(y, sr):
-    return (y)
-
-
-'''------------------------------------
-SILENCE TRIMMER:
-    receives an audio matrix,
-    returns an audio matrix with less silence and the amout of time that was trimmed
-------------------------------------'''
-def trim_silence(y):
-    y_trimmed, index = librosa.effects.trim(y=y, top_db=20, frame_length=2, hop_length=500)
-    trimmed_length = librosa.get_duration(y=y) - librosa.get_duration(y=y_trimmed)
-
-    return y_trimmed, trimmed_length
-
-
-'''------------------------------------
-AUDIO ENHANCER:
-    receives an audio matrix,
-    returns the same matrix after audio manipulation
-------------------------------------'''
-def enhance(y):
-    apply_audio_effects = AudioEffectsChain().lowshelf(gain=10.0, frequency=260, slope=0.1).reverb(reverberance=25, hf_damping=5, room_scale=5, stereo_depth=50, pre_delay=20, wet_gain=0, wet_only=False)#.normalize()
-    y_enhanced = apply_audio_effects(y)
-
-    return y_enhanced
 
 '''------------------------------------
 OUTPUT GENERATOR:
@@ -342,7 +170,7 @@ def run(args, logger):
     
     # generating output files
     for i in range(len(filters[:1])):
-        output_file(i, './01_samples_trimmed_noise_reduced/', args.target, trimmed_y_list[i], sr, args.reference, suffixes[i])
+        output_file(i, './output/', args.target, trimmed_y_list[i], sr, args.reference, suffixes[i])
 
 
 if __name__ == "__main__":
